a7daf8f0dfdb04fef021b1421ca57d2b29736ae8
saac <serch1.alc@gmail.com>
2018-12-03 18:15:31 -0600

    correction in ggplot_bipartite_modules function

--- NetworkAnalysis/src/R_NetworkAnalysis/Functions.R
+++ NetworkAnalysis/src/R_NetworkAnalysis/Functions.R
@@ -76,7 +76,7 @@ ggplot_bipartite_modules <- function(Z, node_data_obs, module_numbers=F, color_t
 #   module_colors <- c('gray50',unique(Set1_modules$color1))
   
   if (weighted){  
-    M %<>% mutate(edge_in_out=ifelse(module1==module2,'in','out')) %>% 
+    M %<>% filter(value!=0) %>% mutate(edge_in_out=ifelse(module1==module2,'in','out')) %>% 
         mutate(value_mod=ifelse(edge_in_out=='in',module1,0)) %>% 
         mutate(Set1=factor(Set1, levels=Set1_modules$Set1), Set2=factor(Set2, levels=Set2_modules$Set2)) 
   } else {    
